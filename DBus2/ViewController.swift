f9c112b993ae61359959916956f874fb2332b51d
natio <paolo.coronati@gmail.com>
2015-07-07 23:33:54 +0100

    Initial Commit

---
+++ DBus2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DBus2
+//
+//  Created by Paolo Coronati on 07/07/15.
+//  Copyright (c) 2015 Paolo Coronati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
